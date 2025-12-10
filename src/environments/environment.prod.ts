42ff1069e8e318f75e2b1d63ef4a9ddd847f23a0
Beforg <bruno.capsqn@gmail.com>
2025-12-09 21:47:11 -0300

    fix environment staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
-  production: true,
+  production: false,
+  staging: true,
   apiUrl: 'https://sua-api-producao.com/api/v1',
   authEndpoint: '/auth',
   barbeirosEndpoint: '/barbeiros',
+  produtosEndpoint: '/produtos',
   servicosEndpoint: '/servicos',
+  perfilEndpoint: '/perfil',
+  agendamentoEndpoint: '/agendamentos',
   IndisponibilidadeEndpoint: '/indisponibilidade'
 };
