8ac8b02123dd3909b1eba8f064d05abae2996fab
Beforg <bruno.capsqn@gmail.com>
2025-12-09 21:44:56 -0300

    add environment staging

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sua-api-producao.com/api/v1',
+  authEndpoint: '/auth',
+  barbeirosEndpoint: '/barbeiros',
+  servicosEndpoint: '/servicos',
+  IndisponibilidadeEndpoint: '/indisponibilidade'
+};
