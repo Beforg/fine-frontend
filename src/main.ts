50aa032ef0bc9ef7ab9090e62b42ed252e552f5e
Beforg <bruno.capsqn@gmail.com>
2025-12-09 23:27:46 -0300

    tentativa de fix comunicação api 3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { baseUrlInterceptor } from './app/core/base-url.interceptor';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(
-      withInterceptors([ baseUrlInterceptor ])
-    )
-  ]
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
