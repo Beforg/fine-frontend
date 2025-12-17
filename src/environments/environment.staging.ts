9bbfac7f751856707afa88337c32769349505167
Beforg <bruno.capsqn@gmail.com>
2025-12-17 13:50:06 -0300

    fix origin HTTP para backend

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   staging: true,
-  apiUrl: 'https://barbearia-fine-api.us-east-2.elasticbeanstalk.com/api/v1',
-  webSocketUrl: 'wss://barbearia-fine-api.us-east-2.elasticbeanstalk.com/ws',
+  apiUrl: 'https://www.barbeariafine.com/api/v1',
+  webSocketUrl: 'wss://www.barbeariafine.com/ws',
   authEndpoint: '/auth',
   barbeirosEndpoint: '/barbeiros',
   produtosEndpoint: '/produtos',
